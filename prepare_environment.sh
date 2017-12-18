09ca276a191de9c6038cf03e8de6d739e22e97a6
Paulo Luan <pauloluan.inova@gmail.com>
2017-12-18 16:35:26 -0200

    removed unused packages

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -8,29 +8,29 @@ apt-get install -y ack-grep
 apt-get install -y chromium-browser
 apt-get install -y curl
 apt-get install -y gcolor2
-apt-get install -y gimp
-apt-get install -y git
+#apt-get install -y gimp
+#apt-get install -y git
 apt-get install -y gitg
-apt-get install -y gummi
-apt-get install -y inkscape
+#apt-get install -y gummi
+#apt-get install -y inkscape
 apt-get install -y meld
-apt-get install -y openjdk-7-jdk
-apt-get install -y python-dev
-apt-get install -y python-setuptools
-apt-get install -y ruby2.0
-apt-get install -y sqlitebrowser
+#apt-get install -y openjdk-7-jdk
+#apt-get install -y python-dev
+#apt-get install -y python-setuptools
+#apt-get install -y ruby2.0
+?#apt-get install -y sqlitebrowser
 apt-get install -y terminator
-apt-get install -y vim-gnome
-apt-get install -y virtualbox
+#apt-get install -y vim-gnome
+#apt-get install -y virtualbox
 
-easy_install pip
+#easy_install pip
 
-pip install ipython
-pip install flake8
-pip install virtualenvwrapper
+#pip install ipython
+#pip install flake8
+#pip install virtualenvwrapper
 
-wget https://atom.io/download/deb
-dpkg -i deb
-rm deb
+#wget https://atom.io/download/deb
+#dpkg -i deb
+#rm deb
 
 apt-get autoclean && apt-get autoremove
