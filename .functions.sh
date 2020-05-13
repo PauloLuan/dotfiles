40e1b3e44c7e757b62fe60c20b152c60d15e0b6d
Paulo Luan <1131963+PauloLuan@users.noreply.github.com>
2020-05-13 15:09:41 -0300

    added commit functions

--- .functions.sh
+++ .functions.sh
@@ -143,3 +143,45 @@ function marathon {
         fi
     fi
 }
+
+#.# Better Git Logs.
+### Using EMOJI-LOG (https://github.com/ahmadawais/Emoji-Log).
+
+# Git Commit, Add all and Push — in one step.
+# function gcap() {
+#     git add . && git commit -m "$*" && git push
+# }
+
+function gcap() {
+    git commit -m "$*"
+}
+
+# NEW.
+function gnew() {
+    gcap "📦 NEW: $@"
+}
+
+# IMPROVE.
+function gimp() {
+    gcap "👌 IMPROVE: $@"
+}
+
+# FIX.
+function gfix() {
+    gcap "🐛 FIX: $@"
+}
+
+# RELEASE.
+function grlz() {
+    gcap "🚀 RELEASE: $@"
+}
+
+# DOC.
+function gdoc() {
+    gcap "📖 DOC: $@"
+}
+
+# TEST.
+function gtst() {
+    gcap "✅ TEST: $@"
+}
